4cdc33d15fcf9e7364de264d805c68d93eece8be
Raivis Olehno <21298759@student.uwl.ac.uk>
2017-09-11 20:13:17 +0100

    Initial Commit

---
+++ Swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Raivis Olehno on 11/09/2017.
+//  Copyright © 2017 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
