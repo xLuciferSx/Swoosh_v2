74835ed4a1e6bd5be32840ec3c77a0591f952d68
Raivis Olehno <21298759@student.uwl.ac.uk>
2017-09-12 00:50:41 +0100

    New changes in View Controller file. Some UX adjustments.

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
